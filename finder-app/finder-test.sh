c7ffd6dc4b5cf2994f6cb4712b9b6086339b81c7
sachinkp21587 <sachinkp21587@gmail.com>
2022-12-05 00:51:04 +0530

    for assignemtn 4.2 test4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,10 +47,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
